0d3b2d2ad5e439df2ecf575cc72ec11e2e8dbf44
Jeongmin <tdcian71@gmail.com>
2021-01-18 02:08:46 +0900

    전체 뷰에 모서리 들어가 있는 상황

--- CornerRadiusForSpecificCorners/ViewController.swift
+++ CornerRadiusForSpecificCorners/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sampleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sampleView.layer.cornerRadius = 15
+        
     }
 
 
